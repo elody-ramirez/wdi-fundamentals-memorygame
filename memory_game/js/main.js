35c0ef5d70b7fd4358045bdf4cd5ec3aa4dfab1b
Ines Primiterio <inesprimiterio@Iness-MBP.home>
2019-05-19 22:12:11 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -21,23 +21,36 @@ cardImage: "images/king-of-diamonds.png",
 }
 ];
 var cardsInPlay = [];
+
 function checkForMatch() {
-	if (cardsInPlay.length === 2){
-		if (cardsInPlay[0] === cardsInPlay[1]) {
-			alert("You found a match!");
-		}
-		else {
-			alert("Sorry, try again.");
-		}
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	}
+	else {
+		alert("Sorry, try again.");
 	}
 }
-function flipCard (cardId) {
+
+function flipCard() {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 	cardsInPlay.push(cards[cardId].rank);
-	checkForMatch();
+	this.setAttribute('src', cards[cardId].cardImage);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
 }
 
-flipCard(0);
-flipCard(3);
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+createBoard();
